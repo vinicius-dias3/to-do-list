6cbc8251fe8f00060c587dc3d733dab5c35ca636
vinicius-dias3 <vinicius.sod3@gmail.com>
2024-03-19 11:18:08 -0300

    adicionando itens e marcando e desmarcando.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,8 +1,8 @@
 const tasks = []
+const taskInput = document.getElementById('taskInput')
 
-function addTask(){
-    const taskInput = document.getElementById('taskInput')
-    const taskText = taskInput.value.trim()
+function addTask(taskText){
+    taskText = taskInput.value.trim()
     //método trim, elimina espaço antes e depois da string
 
     if(taskText !== ''){
@@ -12,34 +12,59 @@ function addTask(){
             completed: false
         };
 
-        tasks.unshift(newTask)
-        taskInput.value = ''
-        console.log(tasks)
-        renderList()
+        tasks.unshift(newTask);
+        taskInput.value = '';
+        renderList();
+        console.log(tasks);
     }
 }
 
 const btnAddTask = document.getElementById('btnAddTask');
 btnAddTask.addEventListener('click', (addTask))
 
+taskInput.addEventListener('keyup', (e)=> {
+    const taskText = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterPressed = key === 13
+
+    if(isEnterPressed) addTask(taskText)
+})
+
 function renderList (){
     const taskList = document.querySelector('#taskList');
     taskList.innerHTML = '';
+
     for(const task of tasks) {
-        taskList.innerHTML += `<li class="task"><button>${task.text}</button></li>`
+        let listItem = document.createElement('li');
+        listItem.textContent = task.text;
+        listItem.addEventListener('click', () => completeTask(listItem, task.id));
+        if(task.completed){
+            listItem.classList.add('completed');
+            console.log(listItem)
+            showBtnMenu(listItem, task);
+        } 
+
+        taskList.appendChild(listItem);
     }
-    // completeTask()
 }
 
-taskList.addEventListener('click', (event)=> {
-    console.log(event)
-    console.log(event.target)
-    if(event.target.parentNode.classList.contains('task')){
-        // event.target.style.color = 'grey';
-        event.target.classList.toggle('completed')
-        // let checkIcon = document.createElement('i')
-        // checkIcon.classList.add('fas', 'fa-check')
-        // event.target.appendChild(checkIcon)
-        // updateTasks()
-    } 
-})
+function completeTask (listItem, taskId){
+    const task = tasks.find(t => {
+        return t.id === taskId
+    });
+    !task.completed ? task.completed = true : task.completed = false;
+    listItem.classList.toggle('completed');
+    console.log(listItem)
+    console.log(task);
+    showBtnMenu(listItem, task)
+}
+
+function showBtnMenu(listItem, task){
+    const btnMenu = document.createElement('button')
+    btnMenu.innerHTML = '<i class="fas fa-ellipsis-v"></i>'
+    
+    task.completed
+    ? listItem.appendChild(btnMenu) 
+    : listItem.removeChild(listItem.childNodes[1]);
+    btnMenu?.addEventListener('click', ()=> console.log('teste'))
+}
