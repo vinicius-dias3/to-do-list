b5f80257e3e597f058ddacb0f4aa87a60753335b
vinicius-dias3 <vinicius.sod3@gmail.com>
2024-03-25 10:25:44 -0300

    adicionado menu de opçoes de tarefa e implementado a funcionalidade de exclusao

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -7,9 +7,9 @@ function addTask(taskText){
 
     if(taskText !== ''){
         const newTask = {
-            id: tasks.length + 1,
+            id: verificarMaiorId() + 1,
             text: taskText,
-            completed: false
+            completed: false,
         };
 
         tasks.unshift(newTask);
@@ -18,6 +18,15 @@ function addTask(taskText){
         console.log(tasks);
     }
 }
+
+
+function verificarMaiorId (){
+    let maiorId = 0
+    for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].id > maiorId) maiorId = tasks[i].id
+    }
+    return maiorId
+};
 
 const btnAddTask = document.getElementById('btnAddTask');
 btnAddTask.addEventListener('click', (addTask))
@@ -36,11 +45,11 @@ function renderList (){
 
     for(const task of tasks) {
         let listItem = document.createElement('li');
-        listItem.textContent = task.text;
-        listItem.addEventListener('click', () => completeTask(listItem, task.id));
+        listItem.innerHTML = `<p>${task.text}</p>`;
+        let taskContent = listItem.querySelector('p')
+        taskContent.addEventListener('click', () => completeTask(listItem, task.id));
         if(task.completed){
             listItem.classList.add('completed');
-            console.log(listItem)
             showBtnMenu(listItem, task);
         } 
 
@@ -49,22 +58,43 @@ function renderList (){
 }
 
 function completeTask (listItem, taskId){
-    const task = tasks.find(t => {
-        return t.id === taskId
-    });
+    const task = tasks.find(t => t.id === taskId);
+
     !task.completed ? task.completed = true : task.completed = false;
     listItem.classList.toggle('completed');
-    console.log(listItem)
-    console.log(task);
     showBtnMenu(listItem, task)
 }
 
 function showBtnMenu(listItem, task){
     const btnMenu = document.createElement('button')
     btnMenu.innerHTML = '<i class="fas fa-ellipsis-v"></i>'
-    
     task.completed
     ? listItem.appendChild(btnMenu) 
     : listItem.removeChild(listItem.childNodes[1]);
-    btnMenu?.addEventListener('click', ()=> console.log('teste'))
+    btnMenu.addEventListener('click', ()=> {
+        renderOption(task, listItem)
+    })
+}
+
+function renderOption(task, listItem){
+    let option = document.createElement('ul')
+    option.classList.add('option')
+    option.innerHTML = `<li><h3>${task.text}</h3></li>
+                        <li id="edit">Edit</li>
+                        <li id="remove">Remove</li>
+                        <li id="search-web">Seach the web</li>
+                        <li id="copy-text">Copy text</li>`
+    listItem.appendChild(option);
+
+    let removeBtn = option.querySelector('#remove')
+    removeBtn.addEventListener('click', ()=> removeTask(listItem, task))
+    option.addEventListener('mouseleave', ()=> listItem.removeChild(option))
+}
+
+function removeTask (listItem, task){
+    listItem.remove(listItem)
+    const index = tasks.findIndex(t => t.id === task.id)
+    if(index !== -1) tasks.splice(index, 1)
+    
+    console.log(tasks)
 }
